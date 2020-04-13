d88f1d92db11af277e3d8919eb5f88d7ee73281c
ttreit <14933277+ttreit@users.noreply.github.com>
2020-04-12 20:41:37 -0700

    In-process recursion practice

--- src/recursion.js
+++ src/recursion.js
@@ -1,21 +1,23 @@
-/* jshint esversion: 6 */
-
-// Solve the following prompts using recursion.
-
-// 1. Calculate the factorial of a number. The factorial of a non-negative integer n,
-// denoted by n!, is the product of all positive integers less than or equal to n.
-// Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
-// factorial(5); // 120
 var factorial = function(n) {
+// if num is greater than 0
+if (n < 0) {
+  return null;
+}
+if (n === 0) {
+  return 1;
+} else {
+  return n * factorial (n - 1);
+}
 };
 
-// 2. Compute the sum of an array of integers.
-// sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
-// 3. Sum all numbers in an array containing nested arrays.
-// arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
 };
 
